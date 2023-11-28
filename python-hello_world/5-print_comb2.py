82dc8da66d78f3c5c9fa9cd638fd54c5214d2273
X-NPC <habtemariamfaris@gmail.com>
2023-11-28 10:35:47 +0300

    let's see

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -3,4 +3,11 @@ number_list = []
 while i != 100:
     number_list.append (i)
     i = i + 1
-print (number_list)
+
+number_list_var = number_list
+print ( "{}".format(number_list_var))
+
+
+#for i in (0,99):
+    #number_list.append(i)
+#print (number_list)
