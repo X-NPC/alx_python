ad817d7ca24d824902fbe73378c7f54d0e5d686c
X-NPC <habtemariamfaris@gmail.com>
2023-11-28 14:08:53 +0300

    misera aymeslegnm

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,11 +1,17 @@
-i = 1
-number_list = "0"
+i = 0
+number_list = ""
 while i != 100:
     number = str(i)
     space_between = ", "
-    number_list = number_list + space_between + number
+    number_list = number_list + number + space_between
     i = i + 1
+    if i == 99:
+        number = str(i)
+        space_between = ""
+        number_list = number_list  + number + space_between
+        i += 1
+    else:
+        None
 
 
 print ( "{}".format(number_list))
-
