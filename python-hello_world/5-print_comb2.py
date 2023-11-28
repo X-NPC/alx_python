4df6ee013fcd301a10fd6489faac0e38f66dab31
X-NPC <habtemariamfaris@gmail.com>
2023-11-28 14:27:42 +0300

    esti eneyew

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,10 +1,15 @@
 i = 0
 number_list = ""
+
 while i != 100:
     number = str(i)
+    #the line below addes a zero in front of numbers less that ten 
+    number = number.zfill(2)
     space_between = ", "
     number_list = number_list + number + space_between
     i = i + 1
+   
+   #the below if statement clears the last unnessary commma (after 99)
     if i == 99:
         number = str(i)
         space_between = ""
