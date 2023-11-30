5b9f5152caa5454d3f255d254335705119dbe3f2
X-NPC <habtemariamfaris@gmail.com>
2023-11-30 10:25:56 +0300

    esti

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -3,9 +3,7 @@ number_list = []
 for i in (0, 100):
     #number = number.zfull(2)
     if i % 10 > i % 100:
-        number = str(i)
-        number = number.zful(2)
-        number_list.append(number) 
+        number_list.append((str(i).zful(2))) 
        
     else:
         None    
