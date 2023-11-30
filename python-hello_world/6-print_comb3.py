67fe167385f29df40be9eae235ba7dfde3d2b076
X-NPC <habtemariamfaris@gmail.com>
2023-11-30 10:30:39 +0300

    esti...

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -3,7 +3,8 @@ number_list = ""
 for i in (0, 100):
     #number = number.zfull(2)
     if i % 10 > i % 100:
-        number_list = number_list + ((str(i).zful(2))) 
+        number_list = number_list + ((str(i).zful(2))) + ", "
+        return number_list
        
     else:
         None    
