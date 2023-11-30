19a14522e8b73797c8f6a450c6eb08e313dfcc36
X-NPC <habtemariamfaris@gmail.com>
2023-11-30 11:34:18 +0300

    still not hopeful

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -10,8 +10,3 @@ def validate_password( password):
         return False
 
   
-
-print(validate_password("Password123"))
-print(validate_password("abc123"))
-print(validate_password("Password 123"))
-print(validate_password("password123"))
