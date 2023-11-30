1133f49eb7640b6153882156780d149fe3c2cfa3
X-NPC <habtemariamfaris@gmail.com>
2023-11-30 11:32:55 +0300

    not-hopeful

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -4,7 +4,7 @@ def validate_password( password):
     condition_two = string_password.isupper() 
     condition_three = string_password.islower()
     condition_four = string_password.isdigit ()
-    if condition_one condition_two and condition_three and condition_four:
+    if condition_one and condition_two and condition_three and condition_four:
         return True
     else:
         return False
