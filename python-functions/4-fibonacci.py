2b1999161a07018b451e5c97ce71b824103239eb
X-NPC <habtemariamfaris@gmail.com>
2023-11-30 11:07:12 +0300

    esti...

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -2,11 +2,11 @@ fibonacci_list = []
 
 def fibonacci_sequence(n):
     if n > 1:
-        for n in (1,):
+        for n in (2,):
             sequence = (n-1) + (n-2)
             fibonacci_list.append (sequence)
     else:
-        fibonacci_list = []
+        None
     return fibonacci_list
 
     
