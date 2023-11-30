5a8a26f444463405b75da9ed9276cbac31f8164b
X-NPC <habtemariamfaris@gmail.com>
2023-11-30 11:40:25 +0300

    lil hope

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,9 +1,10 @@
 def is_prime (number):
-    if number > 0 and number/ N == N != number and number/ N == N != 1 :
-        return False
-    elif number < 0:
-        return False
-    else:
-        return True
+    for i in (1,):
+        if number > 0 and number/ i == i != number and number/ i == i != 1 :
+            return False
+        elif number < 0:
+            return False
+        else:
+            return True
      
     
