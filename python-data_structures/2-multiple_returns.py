a2cdc57ab67e98e9430003967dbc45c9e52dd619
X-NPC <habtemariamfaris@gmail.com>
2023-12-05 14:34:17 +0300

    ahun lamen new?

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,6 +1,11 @@
 def multiple_returns(sentence):
     length = len(sentence)
-    splitted = [ x for x in sentence ]
-    first = splitted [0]
-    return length, first
+    if legth == 0:
+        length = 0
+        first = None
+        return lenght, first
+    else:
+        splitted = [ x for x in sentence ]
+        first = splitted [0]
+        return length, first
 
