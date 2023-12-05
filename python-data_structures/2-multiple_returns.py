126c5492c56a88909a437165905321f8c5cfe37d
X-NPC <habtemariamfaris@gmail.com>
2023-12-05 14:35:25 +0300

    oops

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,6 +1,6 @@
 def multiple_returns(sentence):
     length = len(sentence)
-    if legth == 0:
+    if length == 0:
         length = 0
         first = None
         return lenght, first
@@ -8,4 +8,8 @@ def multiple_returns(sentence):
         splitted = [ x for x in sentence ]
         first = splitted [0]
         return length, first
+    
+sentence = "At Holberton school, I learnt C!"
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))
 
