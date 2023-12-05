eabcdcd0449085aa568736bc6eda92c09f053a79
X-NPC <habtemariamfaris@gmail.com>
2023-12-05 14:43:13 +0300

    eshi!

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -10,6 +10,3 @@ def multiple_returns(sentence):
         return length, first
     
 # zim ble sera sewye!
-sentence = ""
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
