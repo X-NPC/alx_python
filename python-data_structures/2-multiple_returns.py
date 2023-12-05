aa1c2fff39be3c303c9341da7d68d8b3dab01e3a
X-NPC <habtemariamfaris@gmail.com>
2023-12-05 14:36:08 +0300

    min ale bande betsera

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -9,7 +9,4 @@ def multiple_returns(sentence):
         first = splitted [0]
         return length, first
     
-sentence = "At Holberton school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
 
