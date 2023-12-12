4fa9568858d0de5d5fe6bfe7462ef60aa79d4d1a
X-NPC <habtemariamfaris@gmail.com>
2023-12-12 15:23:57 +0300

    sera meselegn

---
+++ python-classes/2-square.py
@@ -0,0 +1,20 @@
+"""
+Does task2 of python classes assignment.
+"""
+class Square:
+    """It improves on 0-squares by raising type error and value error for wrong inputs """
+    
+    def __init__(self, size=0):
+        self._Square__size= size
+        
+        if type(size) != int:
+            print("size must be an integer")
+        elif size < 0:
+            print("size must be >= 0 ")
+        else:
+            pass
+        
+    def area(self):
+        return self._Square__size**2
+    
+
