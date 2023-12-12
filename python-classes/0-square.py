c0746481e0bef8d6da815e497016b80fc3e7d741
X-NPC <habtemariamfaris@gmail.com>
2023-12-12 14:44:15 +0300

    documented

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,7 @@
 class Square:
+    """
+    I don't know how to work this problem but I've tried to create a private attribute(whatever that is).
+    """
     def __init__(self, size):
         self._Square__size= size
         
