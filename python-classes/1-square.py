e22e0aaf585a48a1bf660761da278f06fe69afad
X-NPC <habtemariamfaris@gmail.com>
2023-12-12 15:05:45 +0300

    let's see.

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+"""
+Does task2 of python classes assignment.
+"""
+class Square:
+    """It improves on 0-squares by raising type error and value error for wrong inputs """
+    
+    def __init__(self, size=0):
+        self._Square__size= size
+        if size >= 0:
+            pass
+        else:
+            print("size must be >= 0 ")
+        if type(size) != int:
+            print("size must be an integer")
+        else:
+            pass
