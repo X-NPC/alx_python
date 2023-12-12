3c004f17266bda5eb611a711ef6bda198a2369d3
X-NPC <habtemariamfaris@gmail.com>
2023-12-12 14:47:23 +0300

    doc-ed

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,10 @@
+"""
+I don't know how to work this problem but I've tried to create a private attribute(whatever that is).
+"""
+
 class Square:
     """
-    I don't know how to work this problem but I've tried to create a private attribute(whatever that is).
+    I don't even know what this class does.
     """
     def __init__(self, size):
         self._Square__size= size
