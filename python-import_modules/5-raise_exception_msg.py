e0223b81d23d3117c5aff8734f25852a5bc98645
X-NPC <habtemariamfaris@gmail.com>
2023-12-26 12:30:28 +0300

    I just added the last comment(nothing new)

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -4,3 +4,6 @@ def raise_exception_msg(message= ""):
     return print(message)
 
 #if this works, it means the question has a problem. If it doesn't, the question has an elaboration problem. 
+
+
+#(...after testing) It freakin worked. what the heck...
