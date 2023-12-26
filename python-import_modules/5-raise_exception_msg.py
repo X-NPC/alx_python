e036146e9bd6aa014241011bae7b5315406ef48b
X-NPC <habtemariamfaris@gmail.com>
2023-12-26 12:28:45 +0300

    esti...

---
+++ python-import_modules/5-raise_exception_msg.py
@@ -0,0 +1,6 @@
+"""A function that raises an exception with message"""
+
+def raise_exception_msg(message= ""):
+    return print(message)
+
+#if this works, it means the question has a problem. If it doesn't, the question has an elaboration problem. 
