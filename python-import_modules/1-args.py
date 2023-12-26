114030c9a3fd79e7f0984784db2ecb651cc80b50
X-NPC <habtemariamfaris@gmail.com>
2023-12-26 12:18:55 +0300

    exception raised, indeed.

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,20 @@
+"""This program should be able to list number of arguments"""
+# first I need to know what an argument is
+# then I 
+# use a if statement to determined what to do with 1 argument and otherwise
+# elif --> args==0 print (0 arguments.)
+# else:-->  for loop to iterate in the multiple arguments
+
+# why don't I create a dictionary
+
+def num_of_args ():
+     
+     if len(argv) in __ == 1:
+         output = "1 argument:", newline, "1:", arg
+     elif: len(argv) == 0:
+         output =  
+     else:
+         for i in len(argv):
+             output.append =
+             
+print("{} arguments:")
