876ca4518e32f6f8add9c2525b7c6cd23f668ca0
X-NPC <habtemariamfaris@gmail.com>
2024-01-19 13:55:00 +0300

    ...

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,10 @@
-import add_0
+from add_0 import add
 
 a = 1
 b = 2
 
 def main():
-    return print("{} + {} = {}".format(a, b, add_0.add(a,b)))
+    return print("{} + {} = {}".format(a, b, add(a,b)))
 
 #esti main minlewn awetahut mn endmifeter enayalen
 
