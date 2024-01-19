7e241a21ea0ef0f279328d59c82b6474014fc110
X-NPC <habtemariamfaris@gmail.com>
2024-01-19 17:40:07 +0300

    looks good

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,25 +1,29 @@
 """a function that divides two integers"""
 
 def safe_print_division(a=0, b=1):
+    # custom exception errors when things go wrong (valueError, typeError, denominator=0)
+    try:
+        safe_print_division
+    except ValueError:
+        print ("Please use a valid number")
+    except TypeError:
+        print ("Please input a valid datatype.")
+    except ZeroDivisionError:
+        None
+    finally:
+        print("Inside result: {}" .format(the_result))
+    
+
+
     the_result = a/b
     return the_result
 
+# I didn't know I could put exception in a function till now
 
+# speciall variable from Mesoud
 
-# custom exception errors when things go wrong (valueError, typeError, denominator=0)
-
-try:
-    safe_print_division
-    
-except ValueError:
-    print ("Please use a valid number")
-except TypeError:
-    print ("Please input a valid datatype.")
-except ZeroDivisionError:
-    None
-    
-finally:
-    print("Inside result: {}" .format())
-    
-
-
+if __name__ == "__main__":
+    a = 12
+    b = 2
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
