35b7167a66d68063ce6b1691588008b7c74269f4
X-NPC <habtemariamfaris@gmail.com>
2024-01-19 17:34:02 +0300

    ...

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -5,9 +5,7 @@ def safe_print_division(a=0, b=1):
     return the_result
 
 
-a = 12
-b = 2
-result = safe_print_division(a, b)
+
 # custom exception errors when things go wrong (valueError, typeError, denominator=0)
 
 try:
@@ -21,7 +19,7 @@ except ZeroDivisionError:
     None
     
 finally:
-    print("Inside result: {}" .format(result))
+    print("Inside result: {}" .format())
     
 
-print("{:d} / {:d} = {}".format(a, b, result))
+
