5e4e566706224120540de2c3028f424e10a11f87
X-NPC <habtemariamfaris@gmail.com>
2024-01-19 16:28:46 +0300

    let's see if copying makes a difference.

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -10,20 +10,23 @@
 from sys import argv
 # I didn't know that I needed to import sys lol
 
-def print_arguments ():
-    num_of_args = len(argv) - 1
+
+def print_arguments():
+    num_arguments = len(argv) - 1
+
     
-    print(f"{num_of_args} arguemnt{'s' if num_of_args != 1 else ''}:", end='')
-    
-    if num_of_args == 0:
+    print(f"{num_arguments} argument{'s' if num_arguments != 1 else ''}:", end='')
+
+    if num_arguments == 0:
         print('.')
     else:
         print()
+
         
         for i, arg in enumerate(argv[1:], start=1):
-            print(f"{i}: {arg}") 
-             
+            print(f"{i}: {arg}")
+
 if __name__ == "__main__":
     print_arguments()
-    
+
 
