f1c6adb3cb519a773843a4aa2800dc76791b01e2
X-NPC <habtemariamfaris@gmail.com>
2024-01-19 17:30:17 +0300

    I don't get why it has to contain only 3 lines

--- python-more_data_structures/4-multiply_list_map.py
+++ python-more_data_structures/4-multiply_list_map.py
@@ -1,6 +1,4 @@
-# All of it is copy-pasted from my friend's code so it atleast 
-# needs to be studied and revised 
-
 def multiply_list_map(my_list=[], number=0):
     result_list = list(map(lambda x: x * number, my_list))
-    return result_list
+    return result_list # All of it is copy-pasted from my friend's code so it atleast needs to be studied and revised 
+
