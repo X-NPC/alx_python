8958bdec2c4e5ef8f538b6f7b01159dd88c3c475
X-NPC <habtemariamfaris@gmail.com>
2024-01-25 00:00:25 +0300

    let's see if I'm right

---
+++ python-web_framework/1-hbnb_route.py
@@ -0,0 +1,19 @@
+""" 
+This code should start a flask webapp
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes= False)
+def HellooHbnb():
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes= False)
+def hbnb():
+    return "HBNB!"
+
+if __name__=="__main__":
+    app.run(host= '0.0.0.0', port=5000)
+
+#
