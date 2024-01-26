4ad896ef0f1ddad6ed554c97068a7ecf90752ada
X-NPC <habtemariamfaris@gmail.com>
2024-01-26 10:18:42 +0300

    same

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -1,3 +1,37 @@
 """ 
-relatd to task 2
+related to task 2
 """
+
+""" 
+This following code should imporve on 1-hbnb_route.py file
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes= False)
+def HellooHbnb():
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes= False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route ('/c/<string:text>', strict_slashes= False)
+def c_fun(text):
+    
+    txt= text.replace("_", " ")
+    return ("C {}" .format(txt)) 
+
+#the added part
+@app.route ('/python/<text="is cool">', strict_slashes= False)
+def python_is_cool(text):
+    txt= text.replace("_", " ")
+    return "Python {}" .format(text)
+
+if __name__=="__main__":
+    app.run(host= '0.0.0.0', port=5000)
+
+#
