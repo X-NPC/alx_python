5d5fcad20ce43144366fda0b2eb03fade9ce39db
X-NPC <habtemariamfaris@gmail.com>
2024-02-17 14:30:32 +0300

    my bad

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,27 @@
+    # Display method
+    def display(self):
+        for _ in range(self.y):
+            print()
+        for _ in range(self.height):
+            print(" " * self.x + "#" * self.width)
+
+    # Override __str__ method
+    def __str__(self):
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+
+    # Update method
+    def update(self, *args, **kwargs):
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
