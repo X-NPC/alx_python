b0bc77b68e7eec0d320d780cdd42d9dc0833cf16
X-NPC <habtemariamfaris@gmail.com>
2024-02-17 14:37:34 +0300

    ...

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -3,6 +3,7 @@
 from models.base import Base
 
 class Rectangle(Base):
+    """this class is doc-ed"""
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)  # Call the superclass constructor
         
